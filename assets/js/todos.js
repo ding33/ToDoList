15fbdf67a3d80d76010d6fd632f53d37a1fd8135
ding33 <dding33@gmail.com>
2019-05-21 15:44:49 -0400

    simple to_do_list

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+//check off specific Todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//click on X to delete Todo
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+$("input[type = 'text']").keypress(function(e){
+    if(e.which === 13){
+        //grad the input text
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
